d50ea20d952050c0a3ffd059816b09db2e420e37
Wildrimak <finalwildrimak@gmail.com>
2020-03-24 19:49:55 -0300

    3. Adicionada rota de criar e listar os incidents

---
+++ src/controllers/OngController.js
@@ -0,0 +1,45 @@
+const crypto = require('crypto');
+const connection = require('../database/connection');
+
+/**
+ * Metodos HTTP: GET, POST, PUT, DELETE
+ * Tipos de parâmetros:
+ * 
+ * Query Params: Parâmetros nomeados enviados na rota após "?" (Filtros, paginação)
+ * Route Params: Parâmetros utilizados para identificar recursos
+ * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
+ * 
+ * const params = request.query; //Obtem os parametros da request
+ * const id = request.params; // Obtem os routes params;
+ * const body = request.body; // Obtem o corpo da request;
+ *  
+ */
+
+
+module.exports = {
+
+    async index(request, response) {
+
+        const ongs = await connection('ongs').select('*');
+        return response.json(ongs);
+    
+    },
+
+    async create(request, response) {
+
+        const { name, email, whatsapp, city, uf } = request.body;
+        const id = crypto.randomBytes(4).toString('HEX');
+
+        await connection('ongs').insert({
+            id,
+            name,
+            email,
+            whatsapp, 
+            city, 
+            uf,
+        });
+
+    return response.json({ id });
+
+  }
+}
--- src/routes.js
+++ src/routes.js
@@ -1,47 +1,15 @@
 const express = require('express');
-const crypto = require('crypto');
 
-const connection = require('./database/connection');
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
 
 const routes = express.Router();
 
-/**
- * Metodos HTTP: GET, POST, PUT, DELETE
- * Tipos de parâmetros:
- * 
- * Query Params: Parâmetros nomeados enviados na rota após "?" (Filtros, paginação)
- * Route Params: Parâmetros utilizados para identificar recursos
- * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
- * 
- * const params = request.query; //Obtem os parametros da request
- * const id = request.params; // Obtem os routes params;
- * const body = request.body; // Obtem o corpo da request;
- *  
- */
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
 
-
-routes.get('/ongs', async (request, response) => {
-
-    const ongs = await connection('ongs').select('*');
-    return response.json(ongs);
-
-});
-
-routes.post('/ongs', async (request, response) => {
-  
-    const { name, email, whatsapp, city, uf } = request.body;
-    const id = crypto.randomBytes(4).toString('HEX');
-
-    await connection('ongs').insert({
-        id,
-        name,
-        email,
-        whatsapp, 
-        city, 
-        uf,
-    });
-    return response.json({ id });
-});
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
 
 //Assim que se exporta variaveis de dentro de um arquivo usando o node;
 module.exports = routes;
