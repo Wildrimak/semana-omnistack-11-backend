6830391bcae5f368e42add5d0b1dc754df00fa32
Wildrimak <finalwildrimak@gmail.com>
2020-03-28 16:26:48 -0300

    5. Adicionado validações ao criar uma ong, ao se ver o perfil, ao se ver os casos e ao se deletar um caso

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { errors } = require('celebrate');
 const routes = require('./routes'); //a barra é para dizer que é um arquivo e não um pacote 
 
 const app = express();
@@ -7,5 +8,5 @@ const app = express();
 app.use(cors()); // Diz que aplicação front pode acessar a minha API
 app.use(express.json());
 app.use(routes); //Importante vim após esse app.use(express.json());
-
+app.use(errors());
 app.listen(3333);
