d124acc6a17425b96f684e9046d7fee7fab09492
Wildrimak <finalwildrimak@gmail.com>
2020-03-28 17:02:35 -0300

    5. Criado validação ao se criar um caso, precisa ter um corpo e um header de authorization

--- src/routes.js
+++ src/routes.js
@@ -34,7 +34,16 @@ routes.get('/incidents', celebrate({
     }),
 }), IncidentController.index);
 
-routes.post('/incidents', IncidentController.create);
+routes.post('/incidents',celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required(),
+    }).unknown(),
+    [Segments.BODY]: Joi.object().keys({
+        title: Joi.string().required(),
+        description: Joi.string().required().min(10),
+        value: Joi.number().required().min(0.01),
+    }),
+}), IncidentController.create);
 
 routes.delete('/incidents/:id', celebrate({
     [Segments.PARAMS]: Joi.object().keys({
