8c17a0b3c8b0a40b5f7323a1288f3ad75f727aec
Wildrimak <finalwildrimak@gmail.com>
2020-03-28 19:40:27 -0300

    6. API Adaptada para suportar testes

--- src/app.js
+++ src/app.js
@@ -9,4 +9,5 @@ app.use(cors()); // Diz que aplicação front pode acessar a minha API
 app.use(express.json());
 app.use(routes); //Importante vim após esse app.use(express.json());
 app.use(errors());
-app.listen(3333);
+
+module.exports = app;
